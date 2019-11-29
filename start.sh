78133f1ef3b168f58c296e7864abc2d21ccc6e21
Simon Spiridonov <simon.spiridonov@opuscapita.com>
2019-11-29 17:11:09 +0100

    init

--- start.sh
+++ start.sh
@@ -15,9 +15,19 @@ else
   SUDO="sudo -EHu app"
 fi
 
+if ! grep -q azalist ~/.bash_aliases 2>/dev/null; then
+  cat >> ~/.bash_aliases <<EOF
+alias azalist='az account list --query [].name'
+alias azaset='az account set -s'
+alias azashow='az account show --query name'
+EOF
+fi
+
+cd docker-compose
 if [ "$1" == "build" ]; then
   docker-compose build
   exec $WINPTY docker-compose run --rm ansible
 else
   exec $WINPTY docker-compose run --rm ansible
 fi
+cd ..
