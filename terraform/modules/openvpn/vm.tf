b42fa6646b5750dc91c451faa73d454b4ec563a2
Simon Spiridonov <simon.spiridonov@opuscapita.com>
2019-12-05 14:54:21 +0100

    init

--- terraform/modules/openvpn/vm.tf
+++ terraform/modules/openvpn/vm.tf
@@ -55,16 +55,3 @@ resource "azurerm_virtual_machine" "main" {
   }
 }
 
-resource "null_resource" "ete" {
-
-  provisioner "local-exec" {
-    command = "${path.module}/adduser.sh"
-    environment = {
-      VM_USER = "spiridonov"
-      VM_PUB_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7TnaH2EzW1e2h0kWENXV1BUHV2z93ozws8WtEtc1SsBMPknnhVeV9Bjcg9OGUwM2vUqLFQnVolVjZTzBHfkkZD5I+UcYtd2F08magVv2htSlNhXXPc8ARbTQbIAencnrpeWWLe46SSzT9pbLhaYt6vT0yT/pJIV0MtsOE9y/Sl+7aZwnYpJQuiM/hbMUYew507+JFmV+LZoaPMqMKD3sxuweVgdPKhckSxLdAT4pcESGapdnJeO5xrcQDPsT4sPqutbaEoaCuJaTgGcZePkwIPmHj0p2VO0AOEqk6CBGWEoyJ8PDGDPggD4o2RQTURHWQPGA/tTbJ3GgrMwUg54gRX3igr9Y0x2CK8TmC96rK6b5Ohw2SsR/35g8bJ3OArIK8XDujR0qvsJfGA2gTlUIxdNOGGiY+mmWuBqvFHAsZ9KbaWfkm1p7raRtQ+L7uszLGY4gyyxlzk5vg5Mtn8mUNZjLT+tmlzf5RVEQDMmd37BSl+FSNYJGpQpgwVgYuwae016uRbrTgqTifoL42loDtw4GlobfO5Ibi3DZzqP0IVmVa3qJ2bUn6UB6SITyFIY+Rc1ITIYGeN1vpqYvDpte6bWYFu+kTEBnpnZhgIyjX7wMt3NmXuzkZBN9xcRSuD3eee9gIG+neHE01PM5HLeL8xN5dgHYg7eaNhqUWc4I73w== spiridonov@opuscapita"
-      VM_RG = var.resource_group
-      VM_NAME = var.name
-    }
-    interpreter = ["/bin/bash"]
-  }
-}
