26f5949555740de4ba31f3cd4a517193c629f975
Simon Spiridonov <simon.spiridonov@opuscapita.com>
2019-12-05 14:51:05 +0100

    init

--- terraform/modules/openvpn/adduser.sh
+++ terraform/modules/openvpn/adduser.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -xe
+
+#VM_USER="spiridonov"
+#VM_PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC7TnaH2EzW1e2h0kWENXV1BUHV2z93ozws8WtEtc1SsBMPknnhVeV9Bjcg9OGUwM2vUqLFQnVolVjZTzBHfkkZD5I+UcYtd2F08magVv2htSlNhXXPc8ARbTQbIAencnrpeWWLe46SSzT9pbLhaYt6vT0yT/pJIV0MtsOE9y/Sl+7aZwnYpJQuiM/hbMUYew507+JFmV+LZoaPMqMKD3sxuweVgdPKhckSxLdAT4pcESGapdnJeO5xrcQDPsT4sPqutbaEoaCuJaTgGcZePkwIPmHj0p2VO0AOEqk6CBGWEoyJ8PDGDPggD4o2RQTURHWQPGA/tTbJ3GgrMwUg54gRX3igr9Y0x2CK8TmC96rK6b5Ohw2SsR/35g8bJ3OArIK8XDujR0qvsJfGA2gTlUIxdNOGGiY+mmWuBqvFHAsZ9KbaWfkm1p7raRtQ+L7uszLGY4gyyxlzk5vg5Mtn8mUNZjLT+tmlzf5RVEQDMmd37BSl+FSNYJGpQpgwVgYuwae016uRbrTgqTifoL42loDtw4GlobfO5Ibi3DZzqP0IVmVa3qJ2bUn6UB6SITyFIY+Rc1ITIYGeN1vpqYvDpte6bWYFu+kTEBnpnZhgIyjX7wMt3NmXuzkZBN9xcRSuD3eee9gIG+neHE01PM5HLeL8xN5dgHYg7eaNhqUWc4I73w== spiridonov@opuscapita"
+#VM_RG=RG_Andariel_Infra
+#VM_NAME=openvpn
+
 az vm run-command invoke --command-id RunShellScript -g $VM_RG -n $VM_NAME --scripts "\
 useradd -m $VM_USER \
 && mkdir /home/$VM_USER/.ssh \
-&& echo "$VM_PUB_KEY" > /home/$VM_USER/.ssh/authorized_keys \
+&& echo '$VM_PUB_KEY' > /home/$VM_USER/.ssh/authorized_keys \
 && chown -R $VM_USER:$VM_USER /home/$VM_USER/.ssh \
 && chmod 600 /home/$VM_USER/.ssh/authorized_keys \
 && chmod 700 /home/$VM_USER/.ssh \
--- terraform/modules/openvpn/nsg.tf
+++ terraform/modules/openvpn/nsg.tf
@@ -15,5 +15,17 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "OpenVPN"
+    priority                   = 400
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "UDP"
+    source_port_range          = "*"
+    destination_port_range     = "1194"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = var.common_tags
 }
